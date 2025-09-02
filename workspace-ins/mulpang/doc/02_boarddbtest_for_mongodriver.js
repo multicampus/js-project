77de59b15f22f4c9ffda8742873e63871832f1f2
kilyong <uzoolove@gmail.com>
2025-09-02 13:52:32 +0900

    .

--- workspace-ins/mulpang/doc/02_boarddbtest_for_mongodriver.js
+++ workspace-ins/mulpang/doc/02_boarddbtest_for_mongodriver.js
@@ -9,7 +9,8 @@ const url = 'mongodb://localhost:27017';
 const client = new MongoClient(url);
 
 // Database Name
-const dbName = 'myProject';
+const dbName = 'mulpang';
+// const dbName = 'myProject';
 
 async function main() {
   // Use connect method to connect to the server
@@ -18,12 +19,15 @@ async function main() {
   db = client.db(dbName);
 
   // DB 초기화
-  await db.dropDatabase();
+  // await db.dropDatabase();
 
   db.board = db.collection('board');
+  db.coupon = db.collection('coupon');
 
   // the following code examples can be pasted here...
-  await task();
+  // await task();
+
+  console.log(await todo11());
 
   return 'done.';
 }
@@ -37,21 +41,21 @@ async function task(){
   console.log(await todo1(), '\n');
   console.log(await todo2(), '\n');
   console.log(await todo3(), '\n');
-  // console.log(await todo4(), '\n');
-  // console.log(await todo5(), '\n');
-  // console.log(await todo6(), '\n');
-  // console.log(await todo7(), '\n');
+  console.log(await todo4(), '\n');
+  console.log(await todo5(), '\n');
+  console.log(await todo6(), '\n');
+  console.log(await todo7(), '\n');
   
-  // console.log(await todo8(), '\n');
-  // console.log(await todo2(), '\n');
+  console.log(await todo8(), '\n');
+  console.log(await todo2(), '\n');
 
-  // console.log(await todo9('퍼가요~~~'), '\n');
-  // console.log(await todo9('감사합니다.'), '\n');
-  // console.log(await todo9('ㅇㄷ'), '\n');
-  // console.log(await todo2(), '\n');
+  console.log(await todo9('퍼가요~~~'), '\n');
+  console.log(await todo9('감사합니다.'), '\n');
+  console.log(await todo9('ㅇㄷ'), '\n');
+  console.log(await todo2(), '\n');
 
-  // console.log(await todo10(), '\n');
-  // console.log(await todo2(), '\n');
+  console.log(await todo10(), '\n');
+  console.log(await todo2(), '\n');
 }
 
 // 등록할 게시물
@@ -69,7 +73,8 @@ async function todo1(){
 // find()
 async function todo2(){
   console.log('TODO 2. 모든 board 데이터의 모든 속성 조회');
-  return await db.board.find().toArray();
+  // return await db.board.find().toArray();
+  return util.inspect(await db.board.find().toArray(), { depth: 5 });
 }
 
 // find({검색조건})
@@ -81,31 +86,31 @@ async function todo3(){
 // find({검색조건}, {projection: {출력컬럼}})
 async function todo4(){
   console.log('TODO 4. 모든 board 데이터의 작성자 속성만 조회(_id 포함)');  
-	
+	return await db.board.find({}, { projection: { name: 1 } }).toArray();
 }
 
 // find({검색조건}, {projection: {출력컬럼}})
 async function todo5(){
   console.log('TODO 5. kim이 작성한 게시물의 제목 조회(_id 미포함)');
-  
+  return await db.board.find({ name: 'kim' }, { projection: { _id: 0, title: 1 } }).toArray();
 }
 
 // findOne()
 async function todo6(){
   console.log('TODO 6. 첫번째 게시물 조회(1건)');
-  
+  return await db.board.findOne();
 }
 
 // findOne({검색조건})
 async function todo7(){
   console.log('TODO 7. 게시물 조회(lee가 작성한 데이터 1건 조회)');
-  
+  return await db.board.findOne({ name: 'lee' });
 }
 
 // updateOne({검색조건}, {수정할 문서})
 async function todo8(){
   console.log('TODO 8. 게시물 수정(3번 게시물의 내용 수정)');
-	
+	return await db.board.updateOne({ _id: 3 }, { $set: { content: '수정된 내용' } });
 }
 
 async function todo9(reply){
@@ -114,13 +119,13 @@ async function todo9(reply){
     name: '이영희',
     content: reply
   };
-  
+  return await db.board.updateOne({ _id: 1 }, { $push: { comments: comment } });
 }
 
 // deleteOne({검색 조건})
 async function todo10(){
   console.log('TODO 10. 2번 게시물 삭제');
-	
+  return await db.board.deleteOne({ _id: 2 });
 }
 
 // 1. 모든 쿠폰 목록을 조회한다.
@@ -129,7 +134,7 @@ async function todo10(){
 // 4. 한페이지당 5건 일때 2페이지를 조회.
 async function todo11(){
   console.log('TODO 11. mulpang DB coupon collection 조회');
-  const data = [];
+  const data = await db.coupon.find().project({ couponName: 1, 'saleDate.start': 1 }).toArray();
   
   console.log(data.length, '건 조회됨.');
   return data;
